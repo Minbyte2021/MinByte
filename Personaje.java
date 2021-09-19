a533b94de597282b4e2734d3009b6ae1b5c2ecb4
OscarRuiz9231 <84547660+OscarRuiz9231@users.noreply.github.com>
2021-09-19 09:28:12 -0500

    Add files via upload

---
+++ Personaje.java
@@ -0,0 +1,15 @@
+public abstract class Persona {
+    //Atributos
+    private String id;
+    private String nombre;
+    private String email;
+    private String idioma;
+    private String url;
+
+    public Persona(trin){ 
+        this .nombre = ("Juan ");
+    } 
+
+    //metodo abctracto
+    public abstract void dirigir();
+}
